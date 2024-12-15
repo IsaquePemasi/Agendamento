eb652acd5c987505e50817718689739b31097303
IsaquePemasi <ADSisaque@gmail.com>
2024-12-15 01:47:05 -0300

    updt

---
+++ script_midias.py
@@ -0,0 +1,62 @@
+# pip install pyautogui
+
+import random
+import pyautogui
+import time
+
+# Aguardar 2 segundos antes de começar para dar tempo de colocar o foco na janela correta
+time.sleep(2)
+
+# Pressionar CTRL + V
+pyautogui.hotkey('ctrl', 'o')
+time.sleep(0.5)
+pyautogui.hotkey('ctrl', 'tab')
+time.sleep(0.5)
+pyautogui.hotkey('ctrl', 'tab')
+time.sleep(0.5)
+pyautogui.hotkey('ctrl', 'tab')
+time.sleep(0.5)
+pyautogui.hotkey('ctrl', 'tab')
+time.sleep(0.5)
+pyautogui.hotkey('ctrl', 'tab')
+time.sleep(0.5)
+pyautogui.hotkey('ctrl', 'tab')
+
+    # pyautogui.hotkey('ctrl', 'tab')
+    # pyautogui.hotkey('ctrl', 'tab')
+    # pyautogui.hotkey('ctrl', 'tab')
+    # pyautogui.hotkey('ctrl', 'tab')
+    # pyautogui.hotkey('ctrl', 'tab')
+    # pyautogui.hotkey('ctrl', 'tab')
+    # pyautogui.hotkey('ctrl', 'tab')
+    # pyautogui.hotkey('ctrl', 'tab')
+    # pyautogui.hotkey('ctrl', 'tab')
+    # pyautogui.hotkey('ctrl', 'tab')
+    # pyautogui.hotkey('ctrl', 'tab')
+    # pyautogui.hotkey('ctrl', 'tab')
+    # pyautogui.hotkey('ctrl', 'tab')
+
+
+
+
+
+
+
+# Pressionar CTRL + V
+# pyautogui.hotkey('ctrl', 'v')
+
+# # Pressionar ENTER
+# pyautogui.press('enter')
+
+# # Pressionar ESC três vezes
+# for _ in range(6):
+#     pyautogui.press('backspace')
+
+# # Digitar 2130
+# pyautogui.write('0')
+# pyautogui.write('0')
+# pyautogui.write('3')
+# pyautogui.write(str(random.randint(0, 9)))
+
+# # Pressionar ENTER
+# pyautogui.press('enter')
