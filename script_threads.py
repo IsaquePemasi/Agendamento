f78d52c3e32e2a58ee1f3858e162aedfa12eb28d
IsaquePemasi <ADSisaque@gmail.com>
2024-12-25 23:06:03 -0300

    updt

--- script_threads.py
+++ script_threads.py
@@ -1,24 +1,35 @@
-import pyautogui# Pressionar ESC três vezes
+import pyautogui
+num_tabs = 10
+
 #vai até a postagem
 for _ in range(4):
     pyautogui.press('tab')
+
 # Pressionar ENTER
 pyautogui.press('enter')
+
 # Pressionar CTRL + V
 pyautogui.hotkey('ctrl', 'v')
+
 #vai até a opção de envio de imagem
 pyautogui.press('tab')
 pyautogui.press('enter')    
 
-# Parte pnde vai percorrer as imagens
+# Parte onde vai percorrer as imagens
+print("Selecionando a midia!")
+for _ in range(num_tabs):
+    time.sleep(0.4) 
+    pyautogui.press('tab') 
 
 # Pressionar ENTER
 pyautogui.press('enter')
 
+# percorrer até a postagem
 for _ in range(6):
     pyautogui.press('tab')
 
-# Pressionar ENTER
-pyautogui.press('enter')
+# Postar ENTER
+#pyautogui.press('enter')
 
 pyautogui.hotkey('ctrl', 'r')
+#############################################################################
