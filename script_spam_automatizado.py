390a92466bf03cc56be3726234c45c0bc452bcc4
IsaquePemasi <ADSisaque@gmail.com>
2025-02-15 18:23:26 -0300

    updt

--- script_spam_automatizado.py
+++ script_spam_automatizado.py
@@ -1,6 +1,10 @@
 import pyautogui
 import time
 
+# Move o mouse para a posição (x, y) na tela
+xAgendamento = 1207  # Coordenada X desejada
+yAgendamento = 742  # Coordenada Y desejada
+
 # Faz o scroll para baixo X "cliques"
 # pyautogui.scroll(-1000)
 ###############################################################################################################
@@ -14,3 +18,11 @@ pyautogui.moveTo(x1, y1)
 
 # Realiza um clique na posição atual do mouse
 pyautogui.click()
+
+# Move o mouse para a posição (x, y) na tela
+pyautogui.moveTo(xAgendamento, yAgendamento)
+
+# Realiza um clique na posição atual do mouse
+pyautogui.click()
+
+# quero que execute o script_definitivo.py aqui
