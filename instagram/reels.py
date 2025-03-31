7660b1242607fae7a8fb87426e225cfc8c0c5757
atosaque <isaque.marques@atos.net>
2025-03-30 22:35:54 -0300

    tempo inteiro no video

--- instagram/reels.py
+++ instagram/reels.py
@@ -1,11 +1,14 @@
 import ffmpeg
 
 def add_subtitle_to_video(video_path, subtitle_text, output_path):
+    # Obter a duração do vídeo
+    probe = ffmpeg.probe(video_path)
+    video_duration = float(probe['format']['duration'])
+
     # Temporário: criar um arquivo de legendas SRT
     subtitle_file = 'temp_subtitle.srt'
-    # r'C:\Users\USUARIO\Desktop\Agendamento\instagram\Sophie Rain - Tell Your GF Recoil Dance #shorts.mp4'
     with open(subtitle_file, 'w') as f:
-        f.write(f"1\n00:00:00,000 --> 00:00:10,000\n{subtitle_text}\n")
+        f.write(f"1\n00:00:00,000 --> {int(video_duration // 3600):02}:{int((video_duration % 3600) // 60):02}:{int(video_duration % 60):02},000\n{subtitle_text}\n")
 
     # Usar ffmpeg para adicionar legendas ao vídeo
     (
