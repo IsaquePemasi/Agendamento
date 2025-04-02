18643da11261d55479bf7c80dfeb479eb732b2f5
atosaque <isaque.marques@atos.net>
2025-04-01 23:31:51 -0300

    teste

--- instagram/reels_teste.py
+++ instagram/reels_teste.py
@@ -40,7 +40,7 @@ def add_subtitle_to_video(video_path, subtitle_texts, middle_text, output_dir):
     (
         ffmpeg
         .input(video_path)
-        .filter('subtitles', subtitle_file, force_style='FontName=FonteBonita,PrimaryColour=&H00FFFFFF')  # Adiciona a fonte desejada e cor branca às legendas principais
+        .filter('subtitles', subtitle_file, force_style='FontName=Helvetica,PrimaryColour=&H00FFFFFF')  # Adiciona a fonte desejada e cor branca às legendas principais
         .filter('subtitles', instagram_subtitle_file, force_style='Alignment=6,PrimaryColour=&HC0FFFFFF')  # Alinha as legendas ao topo central e define cor transparente
         .output(output_path)
         .run(overwrite_output=True)
