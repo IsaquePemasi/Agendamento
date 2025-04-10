eff4a42957cfec7ff40fe497974927aae783ffa1
atosaque <isaque.marques@atos.net>
2025-04-10 13:43:01 -0300

    coordenas A e teste de posicap

---
+++ spam_automatizado/perfis/teste_definicao_posicao.py
@@ -0,0 +1,203 @@
+import pyautogui
+import time
+import subprocess
+import random
+
+# Coordenadas fornecidas pelo usuário
+xAgendamento = 1207  # Coordenada X desejada
+yAgendamento = 695  # Coordenada Y desejada
+xAgendamentoDireita = 1250  # Coordenada X desejada
+yAgendamentoDireita = 694  # Coordenada Y desejada
+xVoltar = 167
+yVoltar = 47
+tempo_minimo = 2 #1
+tempo_medio = 2 #2.5
+xDia = 632
+yDia = 434
+x_central = 1072
+y_central = 351
+
+# Definição das variáveis x e y
+x1 = 467  # Coordenada X desejada
+y1 = 229  # Coordenada Y desejada
+x2 = 489  # Coordenada X desejada
+y2 = 311  # Coordenada Y desejada
+x3 = 483  # Coordenada X desejada
+y3 = 400  # Coordenada Y desejada
+x4 = 489  # Coordenada X desejada
+y4 = 384  # Coordenada Y desejada
+x5 = 463  # Coordenada X desejada
+y5 = 359  # Coordenada Y desejada
+x6 = 478  # Coordenada X desejada
+y6 = 339  # Coordenada Y desejada
+x7 = 479  # Coordenada X desejada
+y7 = 325  # Coordenada Y desejada
+x8 = 494  # Coordenada X desejada
+y8 = 298  # Coordenada Y desejada
+x9 = 445  # Coordenada X desejada
+y9 = 285  # Coordenada Y desejada
+x10 = 455  # Coordenada X desejada
+y10 = 268  # Coordenada Y desejada
+x11 = 513  # Coordenada X desejada
+y11 = 242  # Coordenada Y desejada
+x12 = 499  # Coordenada X desejada
+y12 = 220  # Coordenada Y desejada
+x13 = 476  # Coordenada X desejada
+y13 = 201  # Coordenada Y desejada
+x14 = 467  # Coordenada X desejada
+y14 = 188  # Coordenada Y desejada
+x15 = 463  # Coordenada X desejada
+y15 = 163  # Coordenada Y desejada
+x16 = 590  # Coordenada X desejada
+y16 = 246  # Coordenada Y desejada
+x17 = 509  # Coordenada X desejada
+y17 = 394  # Coordenada Y desejada
+x18 = 490  # Coordenada X desejada
+y18 = 551  # Coordenada Y desejada
+x19 = 549  # Coordenada X desejada
+y19 = 706  # Coordenada Y desejada
+x20 = 550  # Coordenada X desejada
+y20 = 927  # Coordenada Y desejada
+
+###################################################################################################
+
+# Movimentos com pyautogui
+time.sleep(1)
+pyautogui.moveTo(x1, y1, duration = 1)
+time.sleep(1)
+pyautogui.moveTo(x_central, y_central, duration = 1)
+time.sleep(1)
+pyautogui.scroll(-10)
+
+time.sleep(1)
+pyautogui.moveTo(x2, y2, duration = 1)
+time.sleep(1)
+pyautogui.moveTo(x_central, y_central, duration = 1)
+time.sleep(1)
+pyautogui.scroll(-10)
+
+time.sleep(1)
+pyautogui.moveTo(x3, y3, duration = 1)
+time.sleep(1)
+pyautogui.moveTo(x_central, y_central, duration = 1)
+time.sleep(1)
+pyautogui.scroll(-150)
+
+time.sleep(1)
+pyautogui.moveTo(x4, y4, duration = 1)
+time.sleep(1)
+pyautogui.moveTo(x_central, y_central, duration = 1)
+time.sleep(1)
+pyautogui.scroll(-150)
+
+time.sleep(1)
+pyautogui.moveTo(x5, y5, duration = 1)
+time.sleep(1)
+pyautogui.moveTo(x_central, y_central, duration = 1)
+time.sleep(1)
+pyautogui.scroll(-150)
+
+time.sleep(1)
+pyautogui.moveTo(x6, y6, duration = 1)
+time.sleep(1)
+pyautogui.moveTo(x_central, y_central, duration = 1)
+time.sleep(1)
+pyautogui.scroll(-150)
+
+time.sleep(1)
+pyautogui.moveTo(x7, y7, duration = 1)
+time.sleep(1)
+pyautogui.moveTo(x_central, y_central, duration = 1)
+time.sleep(1)
+pyautogui.scroll(-150)
+
+time.sleep(1)
+pyautogui.moveTo(x8, y8, duration = 1)
+time.sleep(1)
+pyautogui.moveTo(x_central, y_central, duration = 1)
+time.sleep(1)
+pyautogui.scroll(-150)
+
+time.sleep(1)
+pyautogui.moveTo(x9, y9, duration = 1)
+time.sleep(1)
+pyautogui.moveTo(x_central, y_central, duration = 1)
+time.sleep(1)
+pyautogui.scroll(-150)
+
+time.sleep(1)
+pyautogui.moveTo(x10, y10, duration = 1)
+time.sleep(1)
+pyautogui.moveTo(x_central, y_central, duration = 1)
+time.sleep(1)
+pyautogui.scroll(-150)
+
+time.sleep(1)
+pyautogui.moveTo(x11, y11, duration = 1)
+time.sleep(1)
+pyautogui.moveTo(x_central, y_central, duration = 1)
+time.sleep(1)
+pyautogui.scroll(-150)
+
+time.sleep(1)
+pyautogui.moveTo(x12, y12, duration = 1)
+time.sleep(1)
+pyautogui.moveTo(x_central, y_central, duration = 1)
+time.sleep(1)
+pyautogui.scroll(-150)
+
+time.sleep(1)
+pyautogui.moveTo(x13, y13, duration = 1)
+time.sleep(1)
+pyautogui.moveTo(x_central, y_central, duration = 1)
+time.sleep(1)
+pyautogui.scroll(-150)
+
+time.sleep(1)
+pyautogui.moveTo(x14, y14, duration = 1)
+time.sleep(1)
+pyautogui.moveTo(x_central, y_central, duration = 1)
+time.sleep(1)
+pyautogui.scroll(-150)
+
+time.sleep(1)
+pyautogui.moveTo(x15, y15, duration = 1)
+time.sleep(1)
+pyautogui.moveTo(x_central, y_central, duration = 1)
+time.sleep(1)
+pyautogui.scroll(-150)
+
+time.sleep(1)
+pyautogui.moveTo(x16, y16, duration = 1)
+time.sleep(1)
+pyautogui.moveTo(x_central, y_central, duration = 1)
+time.sleep(1)
+pyautogui.scroll(-150)
+
+time.sleep(1)
+pyautogui.moveTo(x17, y17, duration = 1)
+time.sleep(1)
+pyautogui.moveTo(x_central, y_central, duration = 1)
+time.sleep(1)
+pyautogui.scroll(-150)
+
+time.sleep(1)
+pyautogui.moveTo(x18, y18, duration = 1)
+time.sleep(1)
+pyautogui.moveTo(x_central, y_central, duration = 1)
+time.sleep(1)
+pyautogui.scroll(-150)
+
+time.sleep(1)
+pyautogui.moveTo(x19, y19, duration = 1)
+time.sleep(1)
+pyautogui.moveTo(x_central, y_central, duration = 1)
+time.sleep(1)
+pyautogui.scroll(-150)
+
+time.sleep(1)
+pyautogui.moveTo(x20, y20, duration = 1)
+time.sleep(1)
+pyautogui.moveTo(x_central, y_central, duration = 1)
+time.sleep(1)
+pyautogui.scroll(-150)
